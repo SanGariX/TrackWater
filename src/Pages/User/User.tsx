4776071a97346f275b41cd15e799ff7e94730d37
SanGariX <denya.garkusha.12@gmail.com>
2025-04-22 18:58:59 +0300

    fix: home, user

--- src/Pages/User/User.tsx
+++ src/Pages/User/User.tsx
@@ -1,6 +1,5 @@
 import UserLeft from "../../Components/UserLeft/UserLeft";
 import UserRight from "../../Components/UserRight/UserRight";
-import s from "./User.module.scss";
 const User = () => {
   return (
     <div className={`wrapper`}>
